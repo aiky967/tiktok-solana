9b4cbec2c7189a48fd873ebf9586c2d434f29d92
aiky967 <kesharwania967@gmail.com>
2022-07-23 13:20:40 +0530

    Add: complted rust program

--- frontend/components/Video.js
+++ frontend/components/Video.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useRef, useState } from "react";
 import styles from '../styles/Video.module.css'
 import Comments from "./Comments";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 
 const Video = ({
     address,
@@ -50,12 +52,32 @@ const Video = ({
                 style={{objectFit: 'cover'}}
             />
 
-            {/* Footer */}
+            <Footer 
+                channel = {channel}
+                description = {description}
+                song = {index}
+            />
 
-            {/* Sidebar */}
+            <Sidebar 
+                address = {address}
+                likes = {likes}
+                shares = {shares}
+                onShowComments = {showComments}
+                likeVideo = {likeVideo}
+                index = {index}
+                likesAddress = {likesAddress}
+                messages = {commentCounts}
+            />
 
             {showCommentsModal && (
-                <Comments />
+                <Comments 
+                    onHide = {hideComments}
+                    index = {index}
+                    address = {address}
+                    createComment = {createComment}
+                    getComments = {getComments}
+                    commentCounts = {commentCounts}
+                />
             )}
         </div>
     )
