0b59cc13170566f25e59887e029d276988311e9a
aiky967 <kesharwania967@gmail.com>
2022-07-25 22:22:50 +0530

    Correcton error
    Former-commit-id: 5b4d116b9cb363ca084e8d9e867047bfa3bcbd44

--- frontend/utils.js/const.js
+++ frontend/utils.js/const.js
@@ -1,10 +1,10 @@
-import { clusterApiUrl, PublicKey } from '@solana/web3.js'
-import tiktok from './tiktok_clone.json'
+import { clusterApiUrl, PublicKey } from "@solana/web3.js";
+import tiktok from './tiktok_clone.json';
 
 export const SOLANA_HOST = clusterApiUrl('devnet')
 
-export const TikTok_PROGRAM_ID = new PublicKey(
-    "8aYmhhTSgDZfQ8ka8YagjocQkDhFeCu1YbEVq5PPg8kF"
+export const TIKTOK_PROGRAM_ID = new PublicKey(
+  "8aYmhhTSgDZfQ8ka8YagjocQkDhFeCu1YbEVq5PPg8kF"
 )
 
-export const TIKTOK_IDL = tiktok
+export const TIKTK_IDL = tiktok
--- frontend/utils.js/utils.js
+++ frontend/utils.js/utils.js
@@ -1,21 +1,24 @@
-import * as anchor from '@project-serum/anchor'
-import { WalletNotConnectedError } from '@solana/wallet-adapter-base'
-import { TIKTOK_IDL, TIKTOK_PROGRAM_ID } from './const'
+import * as anchor from '@project-serum/anchor';
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { TIKTOK_IDL, TIKTOK_PROGRAM_ID } from './const';
 
+// This command makes an Lottery
 export function getProgramInstance(connection, wallet) {
-    if(!wallet.publicKey) throw new WalletNotConnectedError()
+  if (!wallet.publicKey) throw new WalletNotConnectedError();
 
-    const provider = new anchor.AnchorProvider(
-        connection,
-        wallet,
-        anchor.AnchorProvider.defaultOptions(),
-    );
+  const provider = new anchor.AnchorProvider(
+    connection,
+    wallet,
+    anchor.AnchorProvider.defaultOptions(),
+  );
+  
+  const idl = TIKTOK_IDL;
 
-    const idl = TIKTOK_IDL
+  // Address of the deployed program.
+  const programId = TIKTOK_PROGRAM_ID;
 
-    const programId = TIKTOK_PROGRAM_ID
+  // Generate the program client from IDL.
+  const program = new(anchor).Program(idl, programId, provider);
 
-    const program = new(anchor).Program(idl, programId, provider);
-
-    return program
+  return program;
 }
