385a3f69c4fcb465dc907614c3873c370e447054
aiky967 <kesharwania967@gmail.com>
2022-07-26 19:41:56 +0530

    Corrected: components
    Former-commit-id: efff456fcc97e43622031a1306f35ce73f97672a

--- frontend/components/BottomBar.js
+++ frontend/components/BottomBar.js
@@ -1,29 +1,25 @@
-import React from "react";
-import styles from '../styles/BottomBar.module.css'
 import { AiFillHome, AiOutlineCompass } from 'react-icons/ai'
 import { IoIosAdd } from 'react-icons/io'
 import { BiMessageMinus } from 'react-icons/bi'
-import { BsPerson } from "react-icons/bs";
+import { BsPerson } from 'react-icons/bs'
+import style from '../styles/BottomBar.module.css'
 
-const BottomBar = ({
-    setNewVideoShow,
-    getTiktoks
-}) => {
-    return (
-        <div className={styles.wrapper}>
-            <AiFillHome className={styles.bottomIcon}/>
-            <AiOutlineCompass className={styles.bottomIcon} onClick={getTiktoks}/>
-            <div className={styles.bottomIcon}>
-                <IoIosAdd 
-                    className={styles.bottomIcon}
-                    onClick={() => setNewVideoShow(true)}
-                    style={{color: 'black'}}
-                />
-            </div>
-            <BiMessageMinus className={styles.bottomIcon}/>
-            <BsPerson className={styles.bottomIcon}/>
-        </div>
-    )
+const BottomBar = ({ setNewVideoShow, getTiktoks }) => {
+  return (
+    <div className={style.wrapper}>
+      <AiFillHome className={style.bottomIcon} />
+      <AiOutlineCompass className={style.bottomIcon} onClick={getTiktoks} />
+      <div className={style.addVideoButton}>
+        <IoIosAdd
+          className={style.bottomIcon}
+          onClick={() => setNewVideoShow(true)}
+          style={{ color: 'black' }}
+        />
+      </div>
+      <BiMessageMinus className={style.bottomIcon} />
+      <BsPerson className={style.bottomIcon} />
+    </div>
+  )
 }
 
-export default BottomBar;
+export default BottomBar
