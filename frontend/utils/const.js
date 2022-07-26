1727e228ae4212fced9be989273ebd46c450d9d0
aiky967 <kesharwania967@gmail.com>
2022-07-26 19:33:33 +0530

    Updated: utils
    Former-commit-id: c6781c276ba3c75f20ad97c48e8a590dd6c44a89

--- frontend/utils/const.js
+++ frontend/utils/const.js
@@ -3,8 +3,9 @@ import tiktok from './tiktok_clone.json';
 
 export const SOLANA_HOST = clusterApiUrl('devnet')
 
+
 export const TIKTOK_PROGRAM_ID = new PublicKey(
   "8aYmhhTSgDZfQ8ka8YagjocQkDhFeCu1YbEVq5PPg8kF"
 )
 
-export const TIKTK_IDL = tiktok
+export const TIKTOK_IDL = tiktok;
